2ddfc66cb237b730162954e33428d23b11b31edb
Miguel Bustamante <mebustamante@telconet.ec>
2025-05-05 16:56:01 -0500

    Fix pyproject.toml bad include config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="SimpleFastCrud",
-    version="0.1",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=["fastapi", "pydantic", "sqlalchemy"],
     entry_points={},
